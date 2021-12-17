417c5fa9d9724fd9c455f49fb4e25da5aa2c9329
Ryoma <takahashiryouma0102@gmail.com>
2021-12-17 21:15:53 +0900

    add practice

--- practice.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", function () {
-  // ヘッダーを変数の中に格納する
-  const header = document.querySelector("header");
-  // 100px以上スクロールしたらヘッダーに「scroll-nav」クラスをつける
-  header.classList.toggle("scroll-nav", window.scrollY > 100);
-});
